2218385ded3723119fe9cb50ae5b0d3bc35de5a6
Peter Snoek <petersnoek@davinci.nl>
2017-02-14 09:30:58 +0100

    added operator. updated function Calculate to use proper operator.

--- Calculator/javascript.js
+++ Calculator/javascript.js
@@ -1,137 +1,119 @@
 
 
-				var getal1=0;
-				var x = '*' ;
-				
+var getal1=0;
+var operator = '';
+
+// clear function
+function Clear(){
+	location.reload();
+}	
+
+// Functie voor knoppen met verschillende cijfers. 
+
+// functie knop 1  	
+function Add1() {
+	document.getElementById('display').innerHTML += '1';
+}			
+
+// functie knop 2   
+function Add2() {
+	document.getElementById('display').innerHTML += '2';
+}			
+
+// functie knop 3   
+function Add3() {
+	document.getElementById('display').innerHTML += '3';
+}			
+
+// functie knop 4
+function Add4() {
+	document.getElementById('display').innerHTML += '4';
+}			
+
+// functie knop 5
+function Add5() {
+	document.getElementById('display').innerHTML += '5';
+}
+
+// functie knop 6
+function Add6() {
+	document.getElementById('display').innerHTML += '6';
+}
+
+// functie knop 7
+function Add7() {
+	document.getElementById('display').innerHTML += '7';
+}
+
+// functie knop 8
+function Add8() {
+	document.getElementById('display').innerHTML += '8';
+}
+
+// functie knop 9
+function Add9() {
+	document.getElementById('display').innerHTML += '9';
+}
+
+//test operators
+/*
+function Plus() {
+	getal1 = parseInt(document.getElementById('display').innerHTML);
+	document.getElementById('display').innerHTML = '+, -, *, /';
+}
+*/
+
+function Plus() {
+	getal1 = parseInt(document.getElementById('display').innerHTML);
+	operator = '+';
+	document.getElementById('display').innerHTML = '';
+}
+
+function Min() {
+	getal1 = parseInt(document.getElementById('display').innerHTML);
+	operator = '-';					
+	document.getElementById('display').innerHTML = '';
+}
+
+function Keer()
+{
+	getal1 = parseInt(document.getElementById('display').innerHTML);
+	operator = '*';
+	document.getElementById('display').innerHTML = '';
+}
+
+function Deel() 
+{ 
+	getal1 = parseInt(document.getElementById('display').innerHTML);
+	operator = '/';
+	document.getElementById('display').innerHTML = '';
+}
+
+//uitkomst berekenen
+function Calculate() {
+	var getalInDisplay = parseInt(document.getElementById('display').innerHTML);
 	
-		// clear function
-				function Clear(){
-					
-					location.reload();
-					
-				}	
+	switch(operator) {
+	    case '+':
+	        var uitkomst = getal1 + getalInDisplay;
+	        break;
+	    case '-':
+	        var uitkomst = getal1 - getalInDisplay;
+	        break;
+	    case '*':
+	        var uitkomst = getal1 * getalInDisplay;
+	        break;
+	    case '/':
+	        var uitkomst = getal1 / getalInDisplay;
+	        break;					        					        
+	    default:
+	        // operator is leeg of iets anders
+	}
+
+	document.getElementById('uitkomst').innerHTML = uitkomst;
+}
+
+	
 
 
 
-
-		// Functie voor knoppen met verschillende cijfers. 
-		
-				// functie knop 1  	
-				function Add1() {
-					document.getElementById('display').innerHTML += '1';
-				}			
-
-
-				// functie knop 2   
-				function Add2() {
-					document.getElementById('display').innerHTML += '2';
-				}			
-
-
-				// functie knop 3   
-				function Add3() {
-					document.getElementById('display').innerHTML += '3';
-				}			
-
-
-				// functie knop 4
-				function Add4() {
-					document.getElementById('display').innerHTML += '4';
-				}			
-
-
-				// functie knop 5
-				function Add5() {
-					document.getElementById('display').innerHTML += '5';
-				}
-
-				// functie knop 6
-				function Add6() {
-					document.getElementById('display').innerHTML += '6';
-				}
-
-
-				// functie knop 7
-				function Add7() {
-					document.getElementById('display').innerHTML += '7';
-				}
-
-
-				// functie knop 8
-				function Add8() {
-					document.getElementById('display').innerHTML += '8';
-				}
-
-				// functie knop 9
-				function Add9() {
-					document.getElementById('display').innerHTML += '9';
-				}
-
-				//test operators
-				/*
-				function Plus() {
-					getal1 = parseInt(document.getElementById('display').innerHTML);
-					document.getElementById('display').innerHTML = '+, -, *, /';
-				}
-				*/
-
-
-				function Plus() {
-					getal1 = parseInt(document.getElementById('display').innerHTML);
-					document.getElementById('display').innerHTML = '+';
-				}
-				
-				function Min() {
-					getal1 = parseInt(document.getElementById('display').innerHTML);
-					document.getElementById('display').innerHTML = '-';
-				}
-				/*
-				function Deel() {
-					getal1 = parseInt(document.getElementById('display').innerHTML);
-					document.getElementById('display').innerHTML = '/';
-				}
-				
-				function Keer() {
-					getal1 = parseInt(document.getElementById('display').innerHTML);
-					document.getElementById('display').innerHTML = '*';
-				}
-				*/
-				
-				
-				function Keer()
-				{
-						
-						
-						var getalInDisplay = parseInt(document.getElementById('display').innerHTML);
-						getal1 = parseInt(document.getElementById('display').innerHTML);
-						document.getElementById('display').innerHTML = '*';
-						
-						
-				}
-
-				function Deel() 
-				{ 
-						getal1 = parseInt(document.getElementById('display').innerHTML);
-						
-						
-						
-						
-						num2 = document.getElementById("getalInDisplay");
-						document.getElementById("uitkomst").innerHTML = getal1 / num2;
-				}
-				
-				
-				
-				
-				//uitkomst berekenen
-				function Calculate() {
-					var getalInDisplay = parseInt(document.getElementById('display').innerHTML);
-					var uitkomst = getal1 + getalInDisplay;
-					var keer = document.getElementById("uitkomst").innerHTML = getal1 * getalInDisplay;
-					document.getElementById('uitkomst').innerHTML = uitkomst;
-				}
-				
-					
-				
-				
-				
